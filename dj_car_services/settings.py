829233039913e7ec9feb6c247cc6a85f7cf05aac
Emilio Ferreyra <emilioferreyra@users.noreply.github.com>
2023-11-07 20:51:41 -0400

    feat(users): Add user model and user management setup
    - Basic user model with custom UserManager added.
    - App 'users' included in Django settings.
    - Initial 'users' app files created.

--- dj_car_services/settings.py
+++ dj_car_services/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
