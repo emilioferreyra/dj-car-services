c9c905c6dcc940f5bd18efbfe76ceed249b41495
Emilio Ferreyra <emilioferreyra@users.noreply.github.com>
2023-11-07 22:06:49 -0400

    feat(admin): Enhanced Django admin interface for User model
    Implemented 'UserAdmin' to customize the Django admin user interface for the User model. This includes use of 'RegistrationForm' for user additions, configuring displayed and editable fields in detail and list views, and setting up search and filter options. This allows for more efficient user model administration.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from sorl.thumbnail.admin import AdminImageMixin
 
-# Register your models here.
+from .forms import RegistrationForm
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(AdminImageMixin, BaseUserAdmin):
+    add_form = RegistrationForm
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'username',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'password1',
+                    'password2',
+                ),
+            },
+        ),
+    )
+    list_display = (
+        'email',
+        'is_staff',
+        'is_active',
+    )
+    fieldsets = (
+        (
+            None,
+            {'fields': ('avatar', 'first_name', 'last_name', 'password')},
+        ),
+        (
+            _('Contact info'),
+            {
+                'fields': (
+                    'email',
+                )
+            },
+        ),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_superuser',
+                    'is_staff',
+
+                )
+            },
+        ),
+        (_('Groups'), {'fields': ('groups', 'user_permissions')}),
+        (
+            _('Important dates'),
+            {'fields': ('last_login', 'date_joined', 'created_date', 'updated_date')},
+        ),
+    )
+    ordering = ('email',)
+    readonly_fields = ('last_login', 'date_joined', 'created_date', 'updated_date')
+    filter_horizontal = ('groups', 'user_permissions')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('is_staff', 'is_active', )
