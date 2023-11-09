530eb669f5db2ea999be1528ce23983af300eed1
Emilio Ferreyra <emilioferreyra@users.noreply.github.com>
2023-11-09 11:24:32 -0400

    feat(admin): Add views for Customer, Employee, Person
    Admin.py is updated to include views for Customer, Employee, and Person. Providing custom display, search functionality, filters, and readonly_fields for these models within Django Admin Interface for a more efficient management and improved user experience.

--- people/admin.py
+++ people/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer
+from .models import Employee
+from .models import Person
+
+
+class CustomBaseAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone')
+    list_display_links = ('first_name', 'last_name', 'email', 'phone')
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by', 'person_type',)
+    list_filter = ('person_type', 'created_at', 'updated_by',)
+    search_fields = ('first_name', 'last_name', 'email', 'phone',)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(CustomBaseAdmin):
+    pass
+
+
+@admin.register(Customer)
+class CustomerAdmin(CustomBaseAdmin):
+    pass
+
+
+@admin.register(Person)
+class PersonAdmin(CustomBaseAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'person_type')
+    list_display_links = ('first_name', 'last_name', 'email', 'phone', 'person_type')
