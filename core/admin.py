81d7e55a722af6ad3a3709b26b08b8e39be35f4b
Emilio Ferreyra <emilioferreyra@users.noreply.github.com>
2023-11-09 22:56:57 -0400

    feat(core-admin): Add UserTimestampMixinAdmin for user tracking
    Introduced an admin model, UserTimestampMixinAdmin, that automatically sets 'created_by' and 'updated_by' fields on save, thus eliminating the need for manual handling.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class UserTimestampMixinAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        obj.save()
