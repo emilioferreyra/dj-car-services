ddbaf7ccf45fcafdd004ce99f7ed33992aa1d6a4
Emilio Ferreyra <emilioferreyra@users.noreply.github.com>
2023-11-10 22:06:37 -0400

    feat(vehicles-admin): Improve interfaces for Vehicle app
    Adjusted the 'vehicles' app's admin.py for enhanced admin controls and cleaner data handling. Introduced new admin classes for 'Make', 'Model', and 'Vehicle' improving their UI on the admin page. The changes enhance descriptiveness, organization, and ensure proper handling of timestamps and vehicle types. Models now include 'vehicle_type', and Vehicles include 'last_reading_mileage'.

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.admin import UserTimestampMixinAdmin
+
+from .models import Make, Model, Vehicle
+
+
+class ModelInline(admin.TabularInline):
+    model = Model
+    extra = 0
+    readonly_fields = ('created_at', 'created_by', 'updated_at', 'updated_by', )
+
+
+@admin.register(Make)
+class MakeAdmin(UserTimestampMixinAdmin):
+    inlines = [ModelInline]
+
+
+@admin.register(Model)
+class ModelAdmin(UserTimestampMixinAdmin):
+    list_display = ('make', 'name', 'vehicle_type')
+    list_filter = ('make', 'vehicle_type')
+    search_fields = ('make', 'name',)
+
+
+@admin.register(Vehicle)
+class VehicleAdmin(UserTimestampMixinAdmin):
+    list_display = ('vin', 'make', 'model', 'year', 'color',)
+    list_filter = ('make', 'model', 'year', 'color',)
+    search_fields = ('vin', 'make', 'model', 'year', 'color',)
+    ordering = ('vin', 'make', 'model', 'year', 'color',)
+    readonly_fields = ('created_by', 'updated_by')
+    raw_id_fields = ('created_by', 'updated_by')
+    date_hierarchy = 'created_at'
