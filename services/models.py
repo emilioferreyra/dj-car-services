af85897e62fe7a2c7a1374f1f7838237fdba08eb
Emilio Ferreyra <emilioferreyra@users.noreply.github.com>
2023-11-10 23:20:43 -0400

    refactor(order-model): Remove 'customer' field
    Removed the 'customer' field from the 'Order' model, simplifying its structure. Now, the customer relationship is managed through the 'Employee' model making the link between 'Order' and 'Customer' redundant. This enhances database architecture and enables more flexible data management. A Django migration was created in line with this schema change.

--- services/models.py
+++ services/models.py
@@ -10,7 +10,6 @@ from .enums import PaymentMethodEnum
 
 class Order(UserTimestampMixin):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     total = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.BooleanField(default=False)
