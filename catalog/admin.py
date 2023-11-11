55cf9ea7013fdb34b14629e477872f3bda856ef7
Emilio Ferreyra <emilioferreyra@users.noreply.github.com>
2023-11-10 22:25:07 -0400

    feat(catalog-model): Enhancements to 'Catalog' model and its admin interface
    - Introduced 'created_at', 'created_by', 'updated_at', and 'updated_by' fields to the 'Catalog' model for better tracking.
    - The 'Catalog' model now inherits from 'UserTimestampMixin' for simplified maintainability.
    - Admin interface improved by adding metadata fields ('name', 'description', 'category') to 'list_display' for enhanced visibility.
    - Introduced a 'list_filter' option for 'category' to facilitate easier navigation in the admin interface.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.admin import UserTimestampMixinAdmin
+
+from .models import Catalog
+
+
+@admin.register(Catalog)
+class CatalogAdmin(UserTimestampMixinAdmin):
+    list_display = ('name', 'description', 'category', )
+    list_filter = ('category', )
+    readonly_fields = ('created_at', 'created_by', 'updated_at', 'updated_by', )
