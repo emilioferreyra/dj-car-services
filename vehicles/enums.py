ace2c1f6adefa738c4a5ecec92c32ab05f6cba28
Emilio Ferreyra <emilioferreyra@users.noreply.github.com>
2023-11-10 22:04:10 -0400

    refactor(VehicleTypeEnum): Align with real world vehicle categories
    The entries of VehicleTypeEnum were refactored to accurately represent real-world vehicle categories, improving classification and search functionality. 'Motorbike' and 'Misc' categories were replaced with 'SUV' and 'Pickup'.

--- vehicles/enums.py
+++ vehicles/enums.py
@@ -4,10 +4,10 @@ from django.utils.translation import gettext_lazy as _
 
 class VehicleTypeEnum(models.IntegerChoices):
     CAR = 1, _('Car')
-    MOTORBIKE = 2, _('Motorbike')
-    TRUCK = 3, _('Truck')
-    VAN = 4, _('Van')
-    BUS = 5, _('Bus')
-    MOTORCYCLE = 6, _('Motorcycle')
-    TRAILER = 7, _('Trailer')
-    MISC = 8, _('Miscellaneous')
+    SUV = 2, _('SUV')
+    PICKUP = 3, _('Pickup')
+    TRUCK = 4, _('Truck')
+    VAN = 5, _('Van')
+    BUS = 6, _('Bus')
+    MOTORCYCLE = 7, _('Motorcycle')
+    TRAILER = 8, _('Trailer')
