44101c43f1066a91b70f2c828594c7b14f317780
Emilio Ferreyra <emilioferreyra@users.noreply.github.com>
2023-11-10 23:33:47 -0400

    feat(url-routing): Redirect from root to admin page
    Implemented a redirect from the root URL of the project to the admin page, easing access for developers and administrators by negating the need to manually append '/admin' to the root URL.

--- dj_car_services/urls.py
+++ dj_car_services/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
+
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/')),
     path('admin/', admin.site.urls),
 ]
